9d9e2bcb6e78638195c996d23af2d7ecf387cd9e
birkin <birkin.diana@gmail.com>
2015-08-12 11:05:28 -0400

    upgrades python version reference.

--- config/wsgi.py
+++ config/wsgi.py
@@ -21,7 +21,7 @@ current_directory = os.path.dirname( os.path.abspath(__file__) )
 ACTIVATE_FILE = os.path.abspath( '%s/../../env_bdpyweb/bin/activate_this.py' % current_directory )
 PROJECT_DIR = os.path.abspath( '%s/../../bdpyweb_code' % current_directory )
 PROJECT_ENCLOSING_DIR = os.path.abspath( '%s/../..' % current_directory )
-SITE_PACKAGES_DIR = os.path.abspath( '%s/../../env_bdpyweb/lib/python2.6/site-packages' % current_directory )
+SITE_PACKAGES_DIR = os.path.abspath( '%s/../../env_bdpyweb/lib/python2.7/site-packages' % current_directory )
 
 ## virtualenv
 execfile( ACTIVATE_FILE, dict(__file__=ACTIVATE_FILE) )  # file loads environmental variables
