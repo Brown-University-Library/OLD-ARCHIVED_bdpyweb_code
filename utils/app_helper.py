3dc74f01b752561a2990fa83846d0a12b519c3a3
birkin <birkin.diana@gmail.com>
2015-11-04 17:01:39 -0500

    adds api key.

--- utils/app_helper.py
+++ utils/app_helper.py
@@ -12,6 +12,108 @@ import requests
 from bdpy import BorrowDirect
 
 
+class EzbHelper( object ):
+    """ Helper functions for app->handle_ezb_v1() """
+
+    def __init__( self, logger ):
+        self.logger = logger
+        self.logger.debug( 'ezb_helper initialized' )
+
+    ## main functions (called by bdpyweb_app.py functions)
+
+    def validate_request( self, params ):
+        """ Checks params, ip, & auth info; returns boolean.
+            Called by bdpyweb_app.handle_v1() """
+        validity = False
+        keys_good = self.check_keys( params )
+        ip_good = self.check_ip()
+        auth_good = self.check_auth( params )
+        if keys_good and ip_good and auth_good:
+            validity = True
+        self.logger.debug( 'validity, `%s`' % validity )
+        return validity
+
+    def do_lookup( self, params ):
+        """ Runs lookup; returns bdpy output.
+            Called by bdpyweb_app.handle_v1() """
+        self.logger.debug( 'params, `%s`' % pprint.pformat(params) )
+        defaults = self.load_bdpy_defaults()
+        bd = BorrowDirect( defaults, self.logger )
+        bd.run_request_item( params['user_barcode'], 'ISBN', params['isbn'] )
+        bdpy_result = bd.request_result
+        self.logger.debug( 'bdpy_result, `%s`' % bdpy_result )
+        return bdpy_result
+
+    def interpret_result( self, bdpy_result ):
+        """ Examines api result and prepares response expected by easyborrow controller.
+            Called by bdpyweb_app.handle_v1()
+            Note: at the moment, it does not appear that the new BD api distinguishes between 'found' and 'requestable'. """
+        return_dct = {
+            'search_result': 'FAILURE', 'bd_confirmation_code': None, 'found': False, 'requestable': False }
+        if 'Request' in bdpy_result.keys():
+            if 'RequestNumber' in bdpy_result['Request'].keys():
+                return_dct['search_result'] = 'SUCCESS'
+                return_dct['bd_confirmation_code'] = bdpy_result['Request']['RequestNumber']
+                return_dct['found'] = True
+                return_dct['requestable'] = True
+        self.logger.debug( 'interpreted result-dct, `%s`' % pprint.pformat(return_dct) )
+        return return_dct
+
+    ## helper functions (called by above functions)
+
+    def load_bdpy_defaults( self ):
+        """ Loads up non-changing bdpy defaults.
+            Called by do_lookup() """
+        defaults = {
+            'API_URL_ROOT': unicode( os.environ['bdpyweb__BDPY_API_ROOT_URL'] ),
+            'API_KEY': unicode( os.environ['bdpyweb__BDPY_API_KEY'] ),
+            'UNIVERSITY_CODE': unicode( os.environ['bdpyweb__BDPY_UNIVERSITY_CODE'] ),
+            'PARTNERSHIP_ID': unicode( os.environ['bdpyweb__BDPY_PARTNERSHIP_ID'] ),
+            'PICKUP_LOCATION': unicode( os.environ['bdpyweb__BDPY_PICKUP_LOCATION'] ),
+            }
+        self.logger.debug( 'defaults, `%s`' % defaults )
+        return defaults
+
+    def check_keys( self, params ):
+        """ Checks required keys; returns boolean.
+            Called by validate_request() """
+        keys_good = False
+        required_keys = [ 'api_authorization_code', 'api_identity', 'isbn',  'user_barcode' ]
+        for required_key in required_keys:
+            if required_key not in params.keys():
+                break
+            if required_key == required_keys[-1]:
+                keys_good = True
+        self.logger.debug( 'keys_good, `%s`' % keys_good )
+        return keys_good
+
+    def check_ip( self ):
+        """ Checks ip; returns boolean.
+            Called by validate_request() """
+        LEGIT_IPS = json.loads( unicode(os.environ['bdpyweb__LEGIT_IPS']))
+        ip_good = False
+        if flask.request.remote_addr in LEGIT_IPS:
+            ip_good = True
+        else:
+            self.logger.debug( 'bad ip, `%s`' % flask.request.remote_addr )
+        self.logger.debug( 'ip_good, `%s`' % ip_good )
+        return ip_good
+
+    def check_auth( self, params ):
+        """ Checks auth params; returns boolean.
+            Called by validate_request() """
+        API_AUTHORIZATION_CODE = unicode( os.environ['bdpyweb__API_AUTHORIZATION_CODE'] )  # for v1
+        API_IDENTITY = unicode( os.environ['bdpyweb__API_IDENTITY'] )  # for v1
+        auth_good = False
+        if params.get( 'api_authorization_code', 'nope' ) == API_AUTHORIZATION_CODE:
+            if params.get( 'api_identity', 'nope' ) == API_IDENTITY:
+                auth_good = True
+        self.logger.debug( 'auth_good, `%s`' % auth_good )
+        return auth_good
+
+    # end class EzbHelper
+
+
 class FormHelper( object ):
     """ Helper functions. """
 
@@ -82,102 +184,102 @@ class FormHelper( object ):
     # end class FormHelper
 
 
-class EzbHelper( object ):
-    """ Helper functions for app->handle_ezb_v1() """
+# class EzbHelper( object ):
+#     """ Helper functions for app->handle_ezb_v1() """
 
-    def __init__( self, logger ):
-        self.logger = logger
-        self.logger.debug( 'ezb_helper initialized' )
+#     def __init__( self, logger ):
+#         self.logger = logger
+#         self.logger.debug( 'ezb_helper initialized' )
 
-    ## main functions (called by bdpyweb_app.py functions)
+#     ## main functions (called by bdpyweb_app.py functions)
 
-    def validate_request( self, params ):
-        """ Checks params, ip, & auth info; returns boolean.
-            Called by bdpyweb_app.handle_v1() """
-        validity = False
-        keys_good = self.check_keys( params )
-        ip_good = self.check_ip()
-        auth_good = self.check_auth( params )
-        if keys_good and ip_good and auth_good:
-            validity = True
-        self.logger.debug( 'validity, `%s`' % validity )
-        return validity
+#     def validate_request( self, params ):
+#         """ Checks params, ip, & auth info; returns boolean.
+#             Called by bdpyweb_app.handle_v1() """
+#         validity = False
+#         keys_good = self.check_keys( params )
+#         ip_good = self.check_ip()
+#         auth_good = self.check_auth( params )
+#         if keys_good and ip_good and auth_good:
+#             validity = True
+#         self.logger.debug( 'validity, `%s`' % validity )
+#         return validity
 
-    def do_lookup( self, params ):
-        """ Runs lookup; returns bdpy output.
-            Called by bdpyweb_app.handle_v1() """
-        defaults = self.load_bdpy_defaults()
-        bd = BorrowDirect( defaults, self.logger )
-        bd.run_request_item( params['user_barcode'], 'ISBN', params['isbn'] )
-        bdpy_result = bd.request_result
-        self.logger.debug( 'bdpy_result, `%s`' % bdpy_result )
-        return bdpy_result
+#     def do_lookup( self, params ):
+#         """ Runs lookup; returns bdpy output.
+#             Called by bdpyweb_app.handle_v1() """
+#         defaults = self.load_bdpy_defaults()
+#         bd = BorrowDirect( defaults, self.logger )
+#         bd.run_request_item( params['user_barcode'], 'ISBN', params['isbn'] )
+#         bdpy_result = bd.request_result
+#         self.logger.debug( 'bdpy_result, `%s`' % bdpy_result )
+#         return bdpy_result
 
-    def interpret_result( self, bdpy_result ):
-        """ Examines api result and prepares response expected by easyborrow controller.
-            Called by bdpyweb_app.handle_v1()
-            Note: at the moment, it does not appear that the new BD api distinguishes between 'found' and 'requestable'. """
-        return_dct = {
-            'search_result': 'FAILURE', 'bd_confirmation_code': None, 'found': False, 'requestable': False }
-        if 'Request' in bdpy_result.keys():
-            if 'RequestNumber' in bdpy_result['Request'].keys():
-                return_dct['search_result'] = 'SUCCESS'
-                return_dct['bd_confirmation_code'] = bdpy_result['Request']['RequestNumber']
-                return_dct['found'] = True
-                return_dct['requestable'] = True
-        self.logger.debug( 'interpreted result-dct, `%s`' % pprint.pformat(return_dct) )
-        return return_dct
+#     def interpret_result( self, bdpy_result ):
+#         """ Examines api result and prepares response expected by easyborrow controller.
+#             Called by bdpyweb_app.handle_v1()
+#             Note: at the moment, it does not appear that the new BD api distinguishes between 'found' and 'requestable'. """
+#         return_dct = {
+#             'search_result': 'FAILURE', 'bd_confirmation_code': None, 'found': False, 'requestable': False }
+#         if 'Request' in bdpy_result.keys():
+#             if 'RequestNumber' in bdpy_result['Request'].keys():
+#                 return_dct['search_result'] = 'SUCCESS'
+#                 return_dct['bd_confirmation_code'] = bdpy_result['Request']['RequestNumber']
+#                 return_dct['found'] = True
+#                 return_dct['requestable'] = True
+#         self.logger.debug( 'interpreted result-dct, `%s`' % pprint.pformat(return_dct) )
+#         return return_dct
 
-    ## helper functions (called by above functions)
+#     ## helper functions (called by above functions)
 
-    def load_bdpy_defaults( self ):
-        """ Loads up non-changing bdpy defaults.
-            Called by do_lookup() """
-        defaults = {
-            'UNIVERSITY_CODE': unicode( os.environ['bdpyweb__BDPY_UNIVERSITY_CODE'] ),
-            'API_URL_ROOT': unicode( os.environ['bdpyweb__BDPY_API_ROOT_URL'] ),
-            'PARTNERSHIP_ID': unicode( os.environ['bdpyweb__BDPY_PARTNERSHIP_ID'] ),
-            'PICKUP_LOCATION': unicode( os.environ['bdpyweb__BDPY_PICKUP_LOCATION'] ),
-            }
-        self.logger.debug( 'defaults, `%s`' % defaults )
-        return defaults
+#     def load_bdpy_defaults( self ):
+#         """ Loads up non-changing bdpy defaults.
+#             Called by do_lookup() """
+#         defaults = {
+#             'UNIVERSITY_CODE': unicode( os.environ['bdpyweb__BDPY_UNIVERSITY_CODE'] ),
+#             'API_URL_ROOT': unicode( os.environ['bdpyweb__BDPY_API_ROOT_URL'] ),
+#             'PARTNERSHIP_ID': unicode( os.environ['bdpyweb__BDPY_PARTNERSHIP_ID'] ),
+#             'PICKUP_LOCATION': unicode( os.environ['bdpyweb__BDPY_PICKUP_LOCATION'] ),
+#             }
+#         self.logger.debug( 'defaults, `%s`' % defaults )
+#         return defaults
 
-    def check_keys( self, params ):
-        """ Checks required keys; returns boolean.
-            Called by validate_request() """
-        keys_good = False
-        required_keys = [ 'api_authorization_code', 'api_identity', 'isbn',  'user_barcode' ]
-        for required_key in required_keys:
-            if required_key not in params.keys():
-                break
-            if required_key == required_keys[-1]:
-                keys_good = True
-        self.logger.debug( 'keys_good, `%s`' % keys_good )
-        return keys_good
+#     def check_keys( self, params ):
+#         """ Checks required keys; returns boolean.
+#             Called by validate_request() """
+#         keys_good = False
+#         required_keys = [ 'api_authorization_code', 'api_identity', 'isbn',  'user_barcode' ]
+#         for required_key in required_keys:
+#             if required_key not in params.keys():
+#                 break
+#             if required_key == required_keys[-1]:
+#                 keys_good = True
+#         self.logger.debug( 'keys_good, `%s`' % keys_good )
+#         return keys_good
 
-    def check_ip( self ):
-        """ Checks ip; returns boolean.
-            Called by validate_request() """
-        LEGIT_IPS = json.loads( unicode(os.environ['bdpyweb__LEGIT_IPS']))
-        ip_good = False
-        if flask.request.remote_addr in LEGIT_IPS:
-            ip_good = True
-        else:
-            self.logger.debug( 'bad ip, `%s`' % flask.request.remote_addr )
-        self.logger.debug( 'ip_good, `%s`' % ip_good )
-        return ip_good
+#     def check_ip( self ):
+#         """ Checks ip; returns boolean.
+#             Called by validate_request() """
+#         LEGIT_IPS = json.loads( unicode(os.environ['bdpyweb__LEGIT_IPS']))
+#         ip_good = False
+#         if flask.request.remote_addr in LEGIT_IPS:
+#             ip_good = True
+#         else:
+#             self.logger.debug( 'bad ip, `%s`' % flask.request.remote_addr )
+#         self.logger.debug( 'ip_good, `%s`' % ip_good )
+#         return ip_good
 
-    def check_auth( self, params ):
-        """ Checks auth params; returns boolean.
-            Called by validate_request() """
-        API_AUTHORIZATION_CODE = unicode( os.environ['bdpyweb__API_AUTHORIZATION_CODE'] )  # for v1
-        API_IDENTITY = unicode( os.environ['bdpyweb__API_IDENTITY'] )  # for v1
-        auth_good = False
-        if params.get( 'api_authorization_code', 'nope' ) == API_AUTHORIZATION_CODE:
-            if params.get( 'api_identity', 'nope' ) == API_IDENTITY:
-                auth_good = True
-        self.logger.debug( 'auth_good, `%s`' % auth_good )
-        return auth_good
+#     def check_auth( self, params ):
+#         """ Checks auth params; returns boolean.
+#             Called by validate_request() """
+#         API_AUTHORIZATION_CODE = unicode( os.environ['bdpyweb__API_AUTHORIZATION_CODE'] )  # for v1
+#         API_IDENTITY = unicode( os.environ['bdpyweb__API_IDENTITY'] )  # for v1
+#         auth_good = False
+#         if params.get( 'api_authorization_code', 'nope' ) == API_AUTHORIZATION_CODE:
+#             if params.get( 'api_identity', 'nope' ) == API_IDENTITY:
+#                 auth_good = True
+#         self.logger.debug( 'auth_good, `%s`' % auth_good )
+#         return auth_good
 
-    # end class Helper()
+#     # end class Helper()
 
